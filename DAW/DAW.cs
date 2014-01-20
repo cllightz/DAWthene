a04c9d0540e649e2229d35ced512d0a89bb59436
狼電 <hazardkirby@gmail.com>
2014-01-21 00:53:29 +0900

    0.2.8 ビルドエラーを修正
    DATA.Noteのエラーを修正

--- DAW/DAW.cs
+++ DAW/DAW.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DATA;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
--- DAW/Note.cs
+++ DAW/Note.cs
@@ -2,30 +2,27 @@
 {
 	class Note
 	{
-		class Note
+		public double Key; //音程
+		public int ST; //音符の長さ
+		public double GT; //発音時間
+		public double Vel; //音量
+
+		public Note(double key, int st, double gt, double vel)
 		{
-			public double Key { get; set; } //音程
-			public int ST { get; set; } //音符の長さ
-			public double GT { get; set; } //発音時間
-			public double Vel { get; set; } //音量
+			//音符
+			Key = key;
+			ST = st;
+			GT = gt;
+			Vel = vel;
+		}
 
-			public Note(double key, int st, double gt, double vel)
-			{
-				//音符
-				Key = key;
-				ST = st;
-				GT = gt;
-				Vel = vel;
-			}
-
-			public Note(int st)
-			{
-				//休符
-				Key = -1;
-				ST = st;
-				GT = 0.0;
-				Vel = 0.0;
-			}
+		public Note(int st)
+		{
+			//休符
+			Key = -1;
+			ST = st;
+			GT = 0.0;
+			Vel = 0.0;
 		}
 	}
 }
